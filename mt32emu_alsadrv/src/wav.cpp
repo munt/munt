65fe14eaa8cbdbc9b1077db7daa3dc9ad9fe6752
Jerome Fisher <re_munt@kingguppy.com>
2011-03-13 21:08:26 +0100

    - Relicensed under LGPL v2.1+.

--- mt32emu_alsadrv/src/wav.cpp
+++ mt32emu_alsadrv/src/wav.cpp
@@ -1,3 +1,21 @@
+/* Copyright (C) 2003 Tristan
+ * Copyright (C) 2004, 2005 Tristan, Jerome Fisher
+ * Copyright (C) 2008, 2011 Tristan, Jerome Fisher, Jörg Walter
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 2.1 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 unsigned char wav_header[45]={
 0x52,0x49,0x46,0x46,0x24,0x00,0xd1,0x00,0x57,0x41,0x56,0x45,0x66,0x6d,0x74,0x20,0x10,0x00,0x00,0x00,
 0x01,0x00,0x02,0x00,0x44,0xac,0x00,0x00,0x10,0xb1,0x02,0x00,0x04,0x00,0x10,0x00,0x64,0x61,0x74,0x61,
