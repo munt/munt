3495792cfd097f2513da2e0e176574444db1392f
Jerome Fisher <re_munt@kingguppy.com>
2011-03-27 08:24:22 +0200

    Added experimental cmake files.

---
+++ mt32emu_smf2wav/cmake/Modules/FindGLIB2.cmake
@@ -0,0 +1,35 @@
+# - Try to find GLib2
+# Once done this will define
+#  GLIB2_FOUND - System has GLib2
+#  GLIB2_INCLUDE_DIRS - The GLib2 include directory
+#  GLIB2_LIBRARIES - Link these to use GLib2
+
+include(FindPkgConfig)
+
+find_package(PkgConfig)
+pkg_search_module(PC_GLIB2 QUIET glib-2.0)
+
+find_path(GLIB2_INCLUDE_DIR glib.h
+  HINTS ${PC_GLIB2_INCLUDEDIR} ${PC_GLIB2_INCLUDE_DIRS}
+  PATH_SUFFIXES glib-2.0
+)
+
+find_path(GLIB2_CONFIG_INCLUDE_DIR
+  glibconfig.h
+  HINTS ${PC_GLIB2_LIBDIR} ${PC_GLIB2_LIBRARY_DIRS}
+  PATH_SUFFIXES lib/glib-2.0/include
+)
+
+find_library(GLIB2_LIBRARY glib-2.0
+  HINTS ${PC_GLIB2_LIBDIR} ${PC_GLIB2_LIBRARY_DIRS}
+)
+
+set(GLIB2_LIBRARIES ${GLIB2_LIBRARY})
+set(GLIB2_INCLUDE_DIRS ${GLIB2_INCLUDE_DIR} ${GLIB2_CONFIG_INCLUDE_DIR})
+
+include(FindPackageHandleStandardArgs)
+# Handle the QUIETLY and REQUIRED arguments and set GLIB2_FOUND to TRUE
+# if all listed variables are TRUE
+find_package_handle_standard_args(GLIB2 DEFAULT_MSG GLIB2_LIBRARY GLIB2_INCLUDE_DIR GLIB2_CONFIG_INCLUDE_DIR)
+
+mark_as_advanced(GLIB2_LIBRARY GLIB2_INCLUDE_DIR GLIB2_CONFIG_INCLUDE_DIR)
---
+++ mt32emu_smf2wav/cmake/Modules/FindMT32EMU.cmake
@@ -0,0 +1,19 @@
+# - Try to find mt32emu
+# Once done this will define
+#  MT32EMU_FOUND - System has mt32emu
+#  MT32EMU_INCLUDE_DIRS - The location of the mt32emu include directory
+#  MT32EMU_LIBRARIES - The location of the mt32emu library
+
+find_path(MT32EMU_INCLUDE_DIR mt32emu/mt32emu.h)
+
+find_library(MT32EMU_LIBRARY mt32emu)
+
+include(FindPackageHandleStandardArgs)
+# Handle the QUIETLY and REQUIRED arguments and set MT32EMU_FOUND to TRUE if
+# all listed variables are TRUE
+find_package_handle_standard_args(MT32EMU DEFAULT_MSG MT32EMU_LIBRARY MT32EMU_INCLUDE_DIR)
+
+set(MT32EMU_LIBRARIES ${MT32EMU_LIBRARY})
+set(MT32EMU_INCLUDE_DIRS ${MT32EMU_INCLUDE_DIR})
+
+mark_as_advanced(MT32EMU_LIBRARY MT32EMU_INCLUDE_DIR)
