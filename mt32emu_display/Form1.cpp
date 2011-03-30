8a842c0ddea8b1f6d85b9a8d850d96eddef234f4
sergm <sergm@bigmir.net>
2011-03-30 12:20:42 +0300

    Updated STA settings for MS VS 2008 Express

--- mt32emu_display/Form1.cpp
+++ mt32emu_display/Form1.cpp
@@ -4,12 +4,12 @@
 
 using namespace mt32emu_display;
 
+[STAThreadAttribute]
 int APIENTRY _tWinMain(HINSTANCE hInstance,
                      HINSTANCE hPrevInstance,
                      LPTSTR    lpCmdLine,
                      int       nCmdShow)
 {
-	System::Threading::Thread::CurrentThread->ApartmentState = System::Threading::ApartmentState::STA;
 	Application::Run(new Form1());
 	return 0;
 }
