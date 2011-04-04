0300237092b1036130c6648036a7e69f9ca3e06f
Serg M <sergm@bigmir.net>
2011-04-04 14:06:55 -0700

    Added constants for new cosine-based WG

--- mt32emu/src/tables.h
+++ mt32emu/src/tables.h
@@ -24,6 +24,10 @@ const int FILTERGRAN = 16000;
 
 const int WGAMP = 16384;
 
+const float RESAMPFACTOR = 512.0f; // Found from sample analysis
+const float RESAMPMAX = 1.75f; // Found from sample analysis
+const float RESAMPFADE = 0.1f; // Found from sample analysis
+
 const int MIDDLEC = 60;
 const int MIDDLEA = 69; // By this I mean "A above middle C"
 
