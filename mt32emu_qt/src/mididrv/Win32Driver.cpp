44acba6c6e2aea9bb8840d8420d6ad0103e22689
sergm <sergm@bigmir.net>
2011-06-06 21:36:13 +0300

    That's even better

--- mt32emu_qt/src/mididrv/Win32Driver.cpp
+++ mt32emu_qt/src/mididrv/Win32Driver.cpp
@@ -53,7 +53,7 @@ LRESULT CALLBACK Win32MidiDriver::MidiInProc(HWND hwnd, UINT uMsg, WPARAM wParam
 			} else if (data[1] == 0) {
 				// Process short MIDI message
 //				std::cout << "Incoming message! msg = " << data[3] << ", timestamp = " << data[2] << "\n";
-				midiSession->getSynthRoute()->pushMIDIShortMessage(data[3], clockSync.sync((qint64)data[2] * NANOS_PER_MILLISECOND));
+				midiSession->getSynthRoute()->pushMIDIShortMessage(data[3], clockSync.sync((qint64)data[2] * MasterClock::NANOS_PER_MILLISECOND));
 				return 1;
 			}
 		} else {
