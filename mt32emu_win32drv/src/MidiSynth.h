1a551d4fa119e510b17ab54f02124f1995daec11
sergm <sergm@bigmir.net>
2014-06-08 11:21:30 +0300

    - Copyright year bump

--- mt32emu_win32drv/src/MidiSynth.h
+++ mt32emu_win32drv/src/MidiSynth.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2011, 2012, 2013 Sergey V. Mikayev
+/* Copyright (C) 2011, 2012, 2013, 2014 Sergey V. Mikayev
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Lesser General Public License as published by
