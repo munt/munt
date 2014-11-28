43c71595437b2fe7ece47f94b9ba1e2dfc6f8190
sergm <sergm@bigmir.net>
2014-11-28 20:52:58 +0200

    - Fixed uninitialised var in MidiSession

--- mt32emu_qt/src/MidiSession.cpp
+++ mt32emu_qt/src/MidiSession.cpp
@@ -18,7 +18,7 @@
 
 using namespace MT32Emu;
 
-MidiSession::MidiSession(QObject *parent, MidiDriver *useMidiDriver, QString useName, SynthRoute *useSynthRoute) : QObject(parent), midiDriver(useMidiDriver), name(useName), synthRoute(useSynthRoute) {
+MidiSession::MidiSession(QObject *parent, MidiDriver *useMidiDriver, QString useName, SynthRoute *useSynthRoute) : QObject(parent), midiDriver(useMidiDriver), name(useName), synthRoute(useSynthRoute), qMidiStreamParser(NULL){
 }
 
 MidiSession::~MidiSession() {
