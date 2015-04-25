d1ed4e28f2c6f86c658f1936459a72887d9f45eb
sergm <sergm@bigmir.net>
2015-04-25 14:32:34 +0300

    - Fixed LinearResampler not remembering the context between successive calls of getOutputSamples() - Improved comments

--- mt32emu_qt/src/resample/LinearResampler.cpp
+++ mt32emu_qt/src/resample/LinearResampler.cpp
@@ -18,17 +18,17 @@
 
 #include <QtGlobal>
 
+#include <cmath>
+
 using namespace MT32Emu;
 
 LinearResampler::LinearResampler(Synth *synth, double targetSampleRate) :
 	SampleRateConverter(synth, targetSampleRate, SampleRateConverter::SRC_FASTEST),
 	inBuffer(new Sample[2 * MAX_SAMPLES_PER_RUN]),
-	position(1.0f), // Effectively makes resampler zero phase
-	lastLeft(),
-	lastRight()
+	inLength(),
+	position(2.0f) // Preload delay line which effectively makes resampler zero phase
 {
 	inBufferPtr = inBuffer;
-	inLength = 0;
 }
 
 LinearResampler::~LinearResampler() {
@@ -40,29 +40,31 @@ Sample LinearResampler::computeOutSample(Sample prev, Sample next) {
 #if MT32EMU_USE_FLOAT_SAMPLES
 	return interpolatedSample;
 #else
-	return Sample(interpolatedSample + ((interpolatedSample < 0) ? -0.5f : +0.5f));
+	// No need to clamp samples here as overshoots are impossible
+	return Sample(interpolatedSample + ((interpolatedSample < 0.0f) ? -0.5f : +0.5f));
 #endif
 }
 
 void LinearResampler::getOutputSamples(Sample *buffer, unsigned int length) {
 	while (length > 0) {
-		if (inLength == 0) {
-			inLength = Bit32u(length * inputToOutputRatio + 0.5);
-			inLength = qBound((Bit32u)1, inLength, (Bit32u)MAX_SAMPLES_PER_RUN);
-			synth->render(inBuffer, inLength);
-			inBufferPtr = inBuffer;
+		while (1.0 <= position) {
+			// Need next sample
+			if (inLength == 0) {
+				inLength = (Bit32u)ceil(length * inputToOutputRatio);
+				inLength = qBound((Bit32u)1, inLength, (Bit32u)MAX_SAMPLES_PER_RUN);
+				synth->render(inBuffer, inLength);
+				inBufferPtr = inBuffer;
+			}
+			--position;
+			lastLeft = nextLeft;
+			nextLeft = *(inBufferPtr++);
+			lastRight = nextRight;
+			nextRight = *(inBufferPtr++);
+			--inLength;
 		}
-		Sample nextLeft = *(inBufferPtr++);
-		Sample nextRight = *(inBufferPtr++);
-		--inLength;
-		while ((position < 1.0) && (length > 0)) {
-			*(buffer++) = computeOutSample(lastLeft, nextLeft);
-			*(buffer++) = computeOutSample(lastRight, nextRight);
-			--length;
-			position += inputToOutputRatio;
-		}
-		--position;
-		lastLeft = nextLeft;
-		lastRight = nextRight;
+		*(buffer++) = computeOutSample(lastLeft, nextLeft);
+		*(buffer++) = computeOutSample(lastRight, nextRight);
+		--length;
+		position += inputToOutputRatio;
 	}
 }
