013e14a4196d7ff360881af94c89a8235df6fce4
sergm <sergm@bigmir.net>
2015-08-02 19:07:21 +0300

    - SoundGroup struct included to 'packed' structures for better portability

--- mt32emu/src/Structures.h
+++ mt32emu/src/Structures.h
@@ -1,5 +1,5 @@
 /* Copyright (C) 2003, 2004, 2005, 2006, 2008, 2009 Dean Beeler, Jerome Fisher
- * Copyright (C) 2011, 2012, 2013, 2014 Dean Beeler, Jerome Fisher, Sergey V. Mikayev
+ * Copyright (C) 2011-2015 Dean Beeler, Jerome Fisher, Sergey V. Mikayev
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Lesser General Public License as published by
@@ -165,6 +165,14 @@ struct MemParams {
 	} MT32EMU_ALIGN_PACKED system;
 };
 
+struct SoundGroup {
+	Bit16u timbreNumberTableAddr;
+	Bit8u displayPosition;
+	Bit8u name[9];
+	Bit8u timbreCount;
+	Bit8u pad;
+} MT32EMU_ALIGN_PACKED;
+
 #if defined(_MSC_VER) || defined(__MINGW32__)
 #pragma pack(pop)
 #else
@@ -240,14 +248,6 @@ struct PatchCache {
 
 	// The following directly points into live sysex-addressable memory
 	const TimbreParam::PartialParam *partialParam;
-};
-
-struct SoundGroup {
-	Bit16u timbreNumberTableAddr;
-	Bit8u displayPosition;
-	Bit8u name[9];
-	Bit8u timbreCount;
-	Bit8u pad;
 };
 
 }
