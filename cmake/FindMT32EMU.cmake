a832f1e16f0ec6e359dffc75418becd761b2912a
sergm <sergm@bigmir.net>
2021-04-04 17:26:31 +0300

    Further improved cmake build
    - Module FindGLIB2.cmake now checks the Glib version is a standard way,
      yet it exports the version found in variable GLIB2_VERSION
    - Improved module FindMT32EMU.cmake so that it now attempts to find
      the library using pkg-config first, also it now permits version checking
      and populates corresponding cmake variables
    - The cmake build script for mt32emu_smf2wav now requires mt32emu version
      2.5.0 to ensure correct compilation

--- cmake/FindMT32EMU.cmake
+++ cmake/FindMT32EMU.cmake
@@ -1,18 +1,37 @@
 # - Try to find mt32emu
 # Once done this will define
 #  MT32EMU_FOUND - System has mt32emu
+#  MT32EMU_VERSION - The version of the mt32emu library
 #  MT32EMU_INCLUDE_DIRS - The location of the mt32emu include directory
 #  MT32EMU_LIBRARIES - The location of the mt32emu library
 
-find_path(MT32EMU_INCLUDE_DIR mt32emu/mt32emu.h)
+find_package(PkgConfig)
+pkg_search_module(PC_MT32EMU QUIET mt32emu)
 
-find_library(MT32EMU_LIBRARY mt32emu)
+find_path(MT32EMU_INCLUDE_DIR mt32emu/mt32emu.h
+  HINTS ${PC_MT32EMU_INCLUDEDIR} ${PC_MT32EMU_INCLUDE_DIRS}
+)
+
+find_library(MT32EMU_LIBRARY mt32emu
+  HINTS ${PC_MT32EMU_LIBDIR} ${PC_MT32EMU_LIBRARY_DIRS}
+)
+
+file(STRINGS ${MT32EMU_INCLUDE_DIR}/mt32emu/config.h MT32EMU_VERSION_DEFINITIONS REGEX "define MT32EMU_VERSION")
+foreach(DEFINITION ${MT32EMU_VERSION_DEFINITIONS})
+  string(REGEX MATCH "(MT32EMU_VERSION[A-Z_]*) *\"?([0-9.]*)\"?" TAGGED_EXPORT ${DEFINITION})
+  set(${CMAKE_MATCH_1} ${CMAKE_MATCH_2})
+endforeach()
+unset(MT32EMU_VERSION_DEFINITIONS)
 
 include(FindPackageHandleStandardArgs)
 # Handle the QUIETLY and REQUIRED arguments and set MT32EMU_FOUND to TRUE if
 # all listed variables are TRUE
-find_package_handle_standard_args(MT32EMU DEFAULT_MSG MT32EMU_LIBRARY MT32EMU_INCLUDE_DIR)
+find_package_handle_standard_args(MT32EMU
+  REQUIRED_VARS MT32EMU_LIBRARY MT32EMU_INCLUDE_DIR
+  VERSION_VAR MT32EMU_VERSION
+)
 
+# FIXME: Populate MT32EMU_EXT_LIBS in static builds properly. Maybe use a Cmake config file instead?
 set(MT32EMU_LIBRARIES ${MT32EMU_LIBRARY} ${MT32EMU_EXT_LIBS})
 set(MT32EMU_INCLUDE_DIRS ${MT32EMU_INCLUDE_DIR})
 
--- mt32emu_smf2wav/cmake/FindGLIB2.cmake
+++ mt32emu_smf2wav/cmake/FindGLIB2.cmake
@@ -1,18 +1,13 @@
 # - Try to find GLib2
 # Once done this will define
 #  GLIB2_FOUND - System has GLib2
+#  GLIB2_VERSION - The version of GLib2 found
 #  GLIB2_INCLUDE_DIRS - The GLib2 include directory
 #  GLIB2_LIBRARIES - Link these to use GLib2
 
 find_package(PkgConfig)
-if(GLIB2_FIND_VERSION)
-  pkg_search_module(PC_GLIB2 QUIET glib-2.0>=${GLIB2_FIND_VERSION})
-  if(PC_GLIB2_FOUND AND (PC_GLIB2_VERSION VERSION_LESS GLIB2_FIND_VERSION))
-    message(FATAL_ERROR "Found GLIB2 version ${PC_GLIB2_VERSION} but ${GLIB2_FIND_VERSION} is required")
-  endif()
-else(GLIB2_FIND_VERSION)
-  pkg_search_module(PC_GLIB2 QUIET glib-2.0)
-endif(GLIB2_FIND_VERSION)
+pkg_search_module(PC_GLIB2 QUIET glib-2.0)
+set(GLIB2_VERSION ${PC_GLIB2_VERSION})
 
 find_path(GLIB2_INCLUDE_DIR glib.h
   HINTS ${PC_GLIB2_INCLUDEDIR} ${PC_GLIB2_INCLUDE_DIRS}
@@ -34,6 +29,9 @@ set(GLIB2_INCLUDE_DIRS ${GLIB2_INCLUDE_DIR} ${GLIB2_CONFIG_INCLUDE_DIR})
 include(FindPackageHandleStandardArgs)
 # Handle the QUIETLY and REQUIRED arguments and set GLIB2_FOUND to TRUE
 # if all listed variables are TRUE
-find_package_handle_standard_args(GLIB2 DEFAULT_MSG GLIB2_LIBRARY GLIB2_INCLUDE_DIR GLIB2_CONFIG_INCLUDE_DIR)
+find_package_handle_standard_args(GLIB2
+  REQUIRED_VARS GLIB2_LIBRARY GLIB2_INCLUDE_DIR GLIB2_CONFIG_INCLUDE_DIR
+  VERSION_VAR GLIB2_VERSION
+)
 
 mark_as_advanced(GLIB2_LIBRARY GLIB2_INCLUDE_DIR GLIB2_CONFIG_INCLUDE_DIR)
