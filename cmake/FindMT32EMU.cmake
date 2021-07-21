0698186558e461d5e25c78bf9ec983fac5778f6a
sergm <sergm@bigmir.net>
2021-07-21 22:16:25 +0300

    Migration to use CMake config file for mt32emu (#65)
    - CMake build script for mt32emu now populates the relevant library target
      properties, so that linking the mt32emu target in a dependent project is
      sufficient for the build to succeed, without necessity to use any extra
      variables.
    - A complete CMake package with configuration, version and targets files is
      now installed onto the target system. The package may include a find-module
      for seeking optional transitive library dependencies.
    - Also, the mt32emu CMake script creates an ALIAS target that follows naming
      convention adopted for IMPORTED targets. This simplifies configuration
      of the other projects further. Additionally, the mt32emu CMake script now
      requires certain versions of the optional dependencies, thus trying its best
      to ensure reliable build.
    - Rewritten find-modules for libraries soxr and samplerate from scratch,
      so that they now behave more uniformly, yet, for convenience, create
      IMPORTED targets instead of a set of variables.
    - Updated the description of the module FindMT32EMU.cmake. It now creates
      the IMPORTED target MT32Emu::mt32emu upon success, similarly to the others.
      A dev-warning is now printed when the find-module is used from a project
      that requires mt32emu 2.6+.

--- cmake/FindMT32EMU.cmake
+++ cmake/FindMT32EMU.cmake
@@ -1,11 +1,42 @@
-# This module is DEPRECATED, do not use. Will be replaced with a proper CMake config & version files in version 2.6.
-# - Try to find mt32emu
-# Once done this will define
-#  MT32EMU_FOUND - System has mt32emu
-#  MT32EMU_VERSION - The version of the mt32emu library
-#  MT32EMU_INCLUDE_DIRS - The location of the mt32emu include directory
-#  MT32EMU_LIBRARIES - The location of the mt32emu library
-#   CAVEAT: MT32EMU_LIBRARIES is missing dependencies in standalone builds when statically linking with mt32emu.
+# This module is DEPRECATED, do not use. Since version 2.6, mt32emu provides CMake config & version files.
+# First, this module checks if target MT32Emu::mt32emu is already known in the build system. In this case,
+# it is assumed that the existing target is a valid dependency and no further checks is performed.
+# Otherwise, the module delegates finding mt32emu to find_package in explicitly set CONFIG mode.
+# The configuration file creates the IMPORTED target MT32Emu::mt32emu, and for backwards compatibility,
+# additionally sets the following variables:
+# * MT32EMU_FOUND - if mt32emu is preset, this variable is TRUE;
+# * MT32EMU_VERSION - the version of the found mt32emu library installation;
+# * MT32EMU_INCLUDE_DIRS - the directory with the mt32emu C/C++ header files;
+# * MT32EMU_LIBRARIES - contains the mt32emu library target for linking.
+# If find_package fails in the CONFIG mode, an attempt is made to locate the mt32emu pkg-config file,
+# and subsequently, to find the link library and the header files in common locations. Upon success,
+# the same IMPORTED target MT32Emu::mt32emu is created and the above set of variables is defined.
+#
+# CAVEAT: If mt32emu is compiled as a static library with external dependencies, there is no easy and
+# reliable way to configure the CMake build system correctly, except by directly using find_package
+# in CONFIG mode and linking with the IMPORTED target MT32Emu::mt32emu.
+
+if(NOT MT32EMU_FIND_VERSION VERSION_LESS 2.6)
+  message(AUTHOR_WARNING "Usage of MT32EMU find-module is DEPRECATED, please use the provided CMake config file directly instead")
+endif()
+
+if(TARGET MT32Emu::mt32emu)
+  if(NOT MT32EMU_FIND_QUIETLY)
+    message("Finding package MT32EMU skipped because target MT32Emu::mt32emu already exists; assuming it is suitable.")
+  endif()
+  set(MT32EMU_FOUND TRUE)
+  set(MT32EMU_LIBRARIES MT32Emu::mt32emu)
+  set(MT32EMU_INCLUDE_DIRS "")
+  return()
+endif()
+
+find_package(MT32Emu ${MT32EMU_FIND_VERSION} QUIET CONFIG)
+if(MT32Emu_FOUND)
+  if(NOT MT32EMU_FIND_QUIETLY)
+    message(STATUS "Found package MT32Emu version ${MT32Emu_VERSION}")
+  endif()
+  return()
+endif()
 
 find_package(PkgConfig)
 pkg_search_module(PC_MT32EMU QUIET mt32emu)
@@ -28,6 +59,8 @@ if(MT32EMU_INCLUDE_DIR AND MT32EMU_LIBRARY)
   unset(MT32EMU_VERSION_DEFINITIONS)
 endif()
 
+mark_as_advanced(MT32EMU_LIBRARY MT32EMU_INCLUDE_DIR)
+
 include(FindPackageHandleStandardArgs)
 # Handle the QUIETLY and REQUIRED arguments and set MT32EMU_FOUND to TRUE if
 # all listed variables are TRUE
@@ -36,7 +69,12 @@ find_package_handle_standard_args(MT32EMU
   VERSION_VAR MT32EMU_VERSION
 )
 
-set(MT32EMU_LIBRARIES ${MT32EMU_LIBRARY} ${MT32EMU_EXT_LIBS})
-set(MT32EMU_INCLUDE_DIRS ${MT32EMU_INCLUDE_DIR})
-
-mark_as_advanced(MT32EMU_EXT_LIBS MT32EMU_LIBRARY MT32EMU_INCLUDE_DIR)
+if(MT32EMU_FOUND)
+  add_library(MT32Emu::mt32emu UNKNOWN IMPORTED)
+  set_target_properties(MT32Emu::mt32emu PROPERTIES
+    INTERFACE_INCLUDE_DIRECTORIES "${MT32EMU_INCLUDE_DIR}"
+    IMPORTED_LOCATION "${MT32EMU_LIBRARY}"
+  )
+  set(MT32EMU_LIBRARIES ${MT32EMU_LIBRARY})
+  set(MT32EMU_INCLUDE_DIRS ${MT32EMU_INCLUDE_DIR})
+endif()
