8731689ccc9877df365b502d4f512ccf9d79d9f3
sergm <sergm@bigmir.net>
2021-08-10 21:24:11 +0300

    Unified configuration of version info for all CMake projects (#65)

---
+++ cmake/project_data.cmake
@@ -0,0 +1,8 @@
+set(munt_DESCRIPTION_SUMMARY "Software synthesiser emulating pre-GM MIDI devices such as Roland MT-32, CM-32L, CM-64 and LAPC-I")
+set(munt_URL "http://munt.sourceforge.net/")
+set(munt_CONTACT "sergm@muntemu.org")
+
+set(munt_VERSION_MAJOR 2)
+set(munt_VERSION_MINOR 6)
+set(munt_VERSION_PATCH 0)
+set(munt_VERSION "${munt_VERSION_MAJOR}.${munt_VERSION_MINOR}.${munt_VERSION_PATCH}")
---
+++ mt32emu_win32drv/cmake/project_data.cmake
@@ -0,0 +1,8 @@
+set(mt32emu_win32drv_DESCRIPTION_SUMMARY "MT32Emu Windows MME MIDI Driver")
+set(mt32emu_win32drv_URL "http://munt.sourceforge.net/")
+set(mt32emu_win32drv_CONTACT "sergm@muntemu.org")
+
+set(mt32emu_win32drv_VERSION_MAJOR 1)
+set(mt32emu_win32drv_VERSION_MINOR 8)
+set(mt32emu_win32drv_VERSION_PATCH 0)
+set(mt32emu_win32drv_VERSION "${mt32emu_win32drv_VERSION_MAJOR}.${mt32emu_win32drv_VERSION_MINOR}.${mt32emu_win32drv_VERSION_PATCH}")
