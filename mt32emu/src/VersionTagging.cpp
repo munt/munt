4ee2dc7ebc23621b0ee777c9808be71d3768e9f3
sergm <sergm@bigmir.net>
2021-08-10 20:54:55 +0300

    Bumped target versions
    - libmt32emu 2.6.0
    - mt32emu-qt 1.10.0
    - mt32emu-smf2wav 1.8.0
    - mt32emu_win32drv 1.8.0
    - munt 2.6.0

--- mt32emu/src/VersionTagging.cpp
+++ mt32emu/src/VersionTagging.cpp
@@ -23,8 +23,9 @@ extern "C" {
 // while for an application linked with a newer library version there will be no match.
 
 MT32EMU_EXPORT_V(2.5) extern const volatile char mt32emu_2_5 = 0;
+MT32EMU_EXPORT_V(2.6) extern const volatile char mt32emu_2_6 = 0;
 
-#if MT32EMU_VERSION_MAJOR > 2 || MT32EMU_VERSION_MINOR > 5
+#if MT32EMU_VERSION_MAJOR > 2 || MT32EMU_VERSION_MINOR > 6
 #error "Missing version tag definition for current library version"
 #endif
 }
--- mt32emu_smf2wav/cmake/project_data.cmake
+++ mt32emu_smf2wav/cmake/project_data.cmake
@@ -3,6 +3,6 @@ set(mt32emu_smf2wav_URL "http://munt.sourceforge.net/")
 set(mt32emu_smf2wav_CONTACT "sergm@muntemu.org")
 
 set(mt32emu_smf2wav_VERSION_MAJOR 1)
-set(mt32emu_smf2wav_VERSION_MINOR 7)
+set(mt32emu_smf2wav_VERSION_MINOR 8)
 set(mt32emu_smf2wav_VERSION_PATCH 0)
 set(mt32emu_smf2wav_VERSION "${mt32emu_smf2wav_VERSION_MAJOR}.${mt32emu_smf2wav_VERSION_MINOR}.${mt32emu_smf2wav_VERSION_PATCH}")
