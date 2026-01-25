b8f7bf10e2eb0ba63588672b4a534a4218362dcd
sergm <sergm@bigmir.net>
2026-01-25 16:14:51 +0100

    mt32emu: Extended ServiceTest to cover reported events handling
    Also added a missing macro cleanup code in c_types.h

--- mt32emu/src/c_interface/c_types.h
+++ mt32emu/src/c_interface/c_types.h
@@ -1,5 +1,5 @@
 /* Copyright (C) 2003, 2004, 2005, 2006, 2008, 2009 Dean Beeler, Jerome Fisher
- * Copyright (C) 2011-2022 Dean Beeler, Jerome Fisher, Sergey V. Mikayev
+ * Copyright (C) 2011-2026 Dean Beeler, Jerome Fisher, Sergey V. Mikayev
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Lesser General Public License as published by
@@ -228,6 +228,7 @@ union mt32emu_report_handler_i {
 
 #undef MT32EMU_REPORT_HANDLER_I_V0
 #undef MT32EMU_REPORT_HANDLER_I_V1
+#undef MT32EMU_REPORT_HANDLER_I_V2
 
 /* === MIDI Receiver Interface === */
 
--- mt32emu/src/test/ServiceTest.cpp
+++ mt32emu/src/test/ServiceTest.cpp
@@ -1,5 +1,5 @@
 /* Copyright (C) 2003, 2004, 2005, 2006, 2008, 2009 Dean Beeler, Jerome Fisher
- * Copyright (C) 2011-2024 Dean Beeler, Jerome Fisher, Sergey V. Mikayev
+ * Copyright (C) 2011-2026 Dean Beeler, Jerome Fisher, Sergey V. Mikayev
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Lesser General Public License as published by
@@ -16,8 +16,11 @@
  */
 
 #include "../ROMInfo.h"
+#include "../Synth.h"
 
 #include "FakeROMs.h"
+#include "TestAccessors.h"
+#include "TestReportHandler.h"
 #include "Testing.h"
 
 #define MT32EMU_API_TYPE 3
@@ -26,7 +29,7 @@
 
 namespace MT32Emu {
 
-template<class ServiceImpl>
+template <class ServiceImpl>
 struct TestService : ServiceImpl {
 	TestService() {}
 
@@ -69,7 +72,7 @@ MT32EMU_STRINGIFY_ENUM(mt32emu_return_code)
 
 namespace MT32Emu {
 
-template<class ServiceImpl>
+template <class ServiceImpl>
 void TestService<ServiceImpl>::addROMSet(const ROMSet &romSet) {
 	const ROMImage *controlROMImage = romSet.getControlROMImage();
 	const ROMImage *pcmROMImage = romSet.getPCMROMImage();
@@ -132,6 +135,158 @@ TEST_CASE_TEMPLATE("Service can open synth with ROMs ", ServiceImpl, TestTypes) 
 
 	service.freeContext();
 	CHECK(service.getContext() == NULL_PTR);
+}
+
+template <class ReportHandlerImpl>
+static ReportHandler3 *ensureNewContextReportHandler(Service &service, ReportHandlerImpl &rh) {
+	service.createContext(rh);
+	REQUIRE(service.getContext() != NULL_PTR);
+	mt32emu_context context = service.getContext();
+	REQUIRE(context != NULL_PTR);
+	ReportHandler3 *res = getReportHandlerDelegate(context);
+	REQUIRE(res != NULL_PTR);
+	return res;
+}
+
+#define MT32EMU_createSubcase(reportedEventType, factory) \
+	SUBCASE(#reportedEventType) { \
+		static const ReportedEvent e = factory; \
+		reportedEvent = &e; \
+	}
+
+TEST_CASE("Service creates ReportHandler that delegates handling of all events to provided interface") {
+	const ReportedEvent *reportedEvent = NULL;
+	MT32EMU_createSubcase(ReportedEvent::DEBUG_MESSAGE, ReportedEvent::debugMessage("Missing PCM ROM"))
+	MT32EMU_createSubcase(ReportedEvent::ERROR_CONTROL_ROM, ReportedEvent::errorControlROM())
+	MT32EMU_createSubcase(ReportedEvent::ERROR_PCM_ROM, ReportedEvent::errorPCMROM())
+	MT32EMU_createSubcase(ReportedEvent::LCD_MESSAGE, ReportedEvent::lcdMessageShown("*** Waiting... ***"))
+	MT32EMU_createSubcase(ReportedEvent::MIDI_MESSAGE, ReportedEvent::midiMessage())
+	MT32EMU_createSubcase(ReportedEvent::MIDI_QUEUE_OVERFLOW, ReportedEvent::midiQueueOverflow())
+	MT32EMU_createSubcase(ReportedEvent::MIDI_SYSTEM_REALTIME, ReportedEvent::midiSystemRealtime(0xFE))
+	MT32EMU_createSubcase(ReportedEvent::DEVICE_RESET, ReportedEvent::deviceReset())
+	MT32EMU_createSubcase(ReportedEvent::DEVICE_RECONFIG, ReportedEvent::deviceReconfig())
+	MT32EMU_createSubcase(ReportedEvent::NEW_REVERB_MODE, ReportedEvent::newReverbMode(2))
+	MT32EMU_createSubcase(ReportedEvent::NEW_REVERB_TIME, ReportedEvent::newReverbTime(3))
+	MT32EMU_createSubcase(ReportedEvent::NEW_REVERB_LEVEL, ReportedEvent::newReverbLevel(7))
+	MT32EMU_createSubcase(ReportedEvent::POLY_STATE_CHANGED, ReportedEvent::polyStateChanged(6))
+	MT32EMU_createSubcase(ReportedEvent::PROGRAM_CHANGED, ReportedEvent::programChanged(3, "Group 5", "Timbre 7"))
+	MT32EMU_createSubcase(ReportedEvent::LCD_STATE_UPDATED, ReportedEvent::lcdStateUpdated())
+	MT32EMU_createSubcase(ReportedEvent::MIDI_MESSAGE_LED_STATE_UPDATED, ReportedEvent::midiMessageLEDStateUpdated(true))
+	MT32EMU_createSubcase(ReportedEvent::NOTE_ON_IGNORED, ReportedEvent::noteOnIgnored(4, 2))
+	MT32EMU_createSubcase(ReportedEvent::PLAYING_POLY_SILENCED, ReportedEvent::playingPolySilenced(3, 1))
+
+	REQUIRE(reportedEvent != NULL_PTR);
+	TestReportHandler<IReportHandlerV2> rh(Array<const ReportedEvent>(reportedEvent, 1));
+	Service service;
+	ReportHandler3 *emitter = ensureNewContextReportHandler(service, rh);
+	switch (reportedEvent->type) {
+	case ReportedEvent::DEBUG_MESSAGE:
+		emitter->printDebug("Missing PCM ROM", 0);
+		break;
+	case ReportedEvent::ERROR_CONTROL_ROM:
+		emitter->onErrorControlROM();
+		break;
+	case ReportedEvent::ERROR_PCM_ROM:
+		emitter->onErrorPCMROM();
+		break;
+	case ReportedEvent::LCD_MESSAGE:
+		emitter->showLCDMessage("*** Waiting... ***");
+		break;
+	case ReportedEvent::MIDI_MESSAGE:
+		emitter->onMIDIMessagePlayed();
+		break;
+	case ReportedEvent::MIDI_QUEUE_OVERFLOW:
+		emitter->onMIDIQueueOverflow();
+		break;
+	case ReportedEvent::MIDI_SYSTEM_REALTIME:
+		emitter->onMIDISystemRealtime(0xFE);
+		break;
+	case ReportedEvent::DEVICE_RESET:
+		emitter->onDeviceReset();
+		break;
+	case ReportedEvent::DEVICE_RECONFIG:
+		emitter->onDeviceReconfig();
+		break;
+	case ReportedEvent::NEW_REVERB_MODE:
+		emitter->onNewReverbMode(2);
+		break;
+	case ReportedEvent::NEW_REVERB_TIME:
+		emitter->onNewReverbTime(3);
+		break;
+	case ReportedEvent::NEW_REVERB_LEVEL:
+		emitter->onNewReverbLevel(7);
+		break;
+	case ReportedEvent::POLY_STATE_CHANGED:
+		emitter->onPolyStateChanged(6);
+		break;
+	case ReportedEvent::PROGRAM_CHANGED:
+		emitter->onProgramChanged(3, "Group 5", "Timbre 7");
+		break;
+	case ReportedEvent::LCD_STATE_UPDATED:
+		emitter->onLCDStateUpdated();
+		break;
+	case ReportedEvent::MIDI_MESSAGE_LED_STATE_UPDATED:
+		emitter->onMidiMessageLEDStateUpdated(true);
+		break;
+	case ReportedEvent::NOTE_ON_IGNORED:
+		emitter->onNoteOnIgnored(4, 2);
+		break;
+	case ReportedEvent::PLAYING_POLY_SILENCED:
+		emitter->onPlayingPolySilenced(3, 1);
+		break;
+	default:
+		FAIL("Unknown event type" << reportedEvent->type);
+		break;
+	}
+
+	rh.checkRemainingEvents();
+}
+
+#undef MT32EMU_createSubcase
+
+TEST_CASE("Service creates ReportHandler that ignores events not supported by provided interface V0") {
+	const ReportedEvent events[] = { ReportedEvent::deviceReset() };
+	TestReportHandler<IReportHandler> rh(events);
+	Service service;
+	ReportHandler3 *emitter = ensureNewContextReportHandler(service, rh);
+	emitter->onNoteOnIgnored(4, 2);
+	emitter->onLCDStateUpdated();
+	emitter->onDeviceReset();
+
+	rh.checkRemainingEvents();
+}
+
+TEST_CASE("Service creates ReportHandler that ignores events not supported by provided interface V1") {
+	const ReportedEvent events[] = { ReportedEvent::lcdStateUpdated(), ReportedEvent::deviceReset() };
+	TestReportHandler<IReportHandlerV1> rh(events);
+	Service service;
+	ReportHandler3 *emitter = ensureNewContextReportHandler(service, rh);
+	emitter->onNoteOnIgnored(4, 2);
+	emitter->onLCDStateUpdated();
+	emitter->onDeviceReset();
+
+	rh.checkRemainingEvents();
+}
+
+TEST_CASE("Service creates ReportHandler that ignores events if it cannot deliver them to provided interface") {
+	struct Inner {
+		static mt32emu_report_handler_version MT32EMU_C_CALL getVersionID(mt32emu_report_handler_i) {
+			return MT32EMU_REPORT_HANDLER_VERSION_CURRENT;
+		}
+
+		mt32emu_report_handler_i_v2 rh;
+
+		Inner() : rh() {
+			rh.getVersionID = getVersionID;
+		}
+	} wrapper;
+	mt32emu_report_handler_i rrh;
+	rrh.v2= &wrapper.rh;
+	Service service;
+	ReportHandler3 *emitter = ensureNewContextReportHandler(service, rrh);
+	emitter->onNoteOnIgnored(4, 2);
+	emitter->onLCDStateUpdated();
+	emitter->onDeviceReset();
 }
 
 } // namespace Test
